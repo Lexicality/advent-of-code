dded39c70f0431767cb493a9b318b30f8a1e0d94
Lex Robinson <lex@lex.me.uk>
2020-12-05 15:24:45 +0000

    Choose which year to run
    And thus it is revealed - the entire point of this elaborate framework - YEARS!

--- src/aoc/__main__.py
+++ src/aoc/__main__.py
@@ -17,25 +17,31 @@ def main():
     path = os.path.dirname(os.path.abspath(filename))
     here = pathlib.Path(path)
 
-    available_days = list(p.stem for p in here.glob("day*.py"))
-    available_days.sort()
-
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog="python -m aoc")
     parser.add_argument("--example", default=False, action="store_true")
-    parser.add_argument(
-        "day",
-        type=int,
-        choices=[int(day.replace("day", "")) for day in available_days],
-        help="Which day's code to run",
-    )
+
+    year_args = parser.add_subparsers(dest="year")
+
+    for year in here.glob("y*"):
+        available_days = list(p.stem for p in year.glob("day*.py"))
+        available_days.sort()
+
+        year_parser = year_args.add_parser(year.stem.replace("y", ""))
+        year_parser.add_argument(
+            "day",
+            type=int,
+            choices=[int(day.replace("day", "")) for day in available_days],
+            help="Which day's code to run",
+        )
+
     args = parser.parse_args()
 
     day_data_file = pathlib.Path(
-        "data", "example" if args.example else ".", f"{args.day}.txt"
+        "data", args.year, "example" if args.example else ".", f"{args.day}.txt"
     ).resolve()
     day_data = read_file(day_data_file)
 
-    day = importlib.import_module(f"aoc.day{args.day}")
+    day = importlib.import_module(f"aoc.y{args.year}.day{args.day}")
     day.main(day_data)
 
 
