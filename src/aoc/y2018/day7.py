f1edad81cc3a43edf0a7ab9969d658bd5fbbf189
Lex Robinson <lex@lex.me.uk>
2020-12-05 17:45:18 +0000

    2018 7.2 incorrect

--- src/aoc/y2018/day7.py
+++ src/aoc/y2018/day7.py
@@ -1,22 +1,47 @@
 import itertools
 import re
-from typing import Iterable, List, Set, Tuple
+from pprint import pprint
+from typing import Iterable, List, Optional, Set, Tuple
+
+NUM_WORKERS = 2
+JOB_TIME = 0 - ord("A") + 1
+
+
+class Worker:
+    name: str
+    job: Optional["Step"] = None
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self) -> str:
+        rep = f"Worker #{self.name} "
+        if self.job:
+            rep += f"Working on {self.job.name}"
+        else:
+            rep += "(idle!)"
+        return rep
 
 
 class Step:
     name: str
     dependencies: Set["Step"]
     dependants: Set["Step"]
-    done = False
+    timeLeft: int
+    worker: Optional[Worker] = None
 
     def __init__(self, name: str):
         self.name = name
         self.dependencies = set()
         self.dependants = set()
+        self.timeLeft = JOB_TIME + ord(name)
 
     def __repr__(self) -> str:
         deps = ",".join(step.name for step in self.dependencies)
-        return f"Step {self.name} [{deps}]"
+        rep = f"Step {self.name} [{deps}]"
+        if self.worker:
+            rep += f" (Being worked on by #{self.worker.name}, {self.timeLeft:,} seconds left)"
+        return rep
 
 
 STEP_RE = re.compile(r"Step (.) must be finished before step (.) can begin.")
@@ -38,30 +63,50 @@ def main(data: Iterable[str]):
         for step in sorted(set(itertools.chain.from_iterable(raw_steps)))
     }
 
-    print(raw_steps)
-
     for dependency, step_name in raw_steps:
         dep_step = all_steps[dependency]
         step = all_steps[step_name]
         step.dependencies.add(dep_step)
         dep_step.dependants.add(step)
 
-    print(all_steps)
+    workers = [Worker(i + 1) for i in range(NUM_WORKERS)]
 
-    order = ""
+    time = 0
 
     while len(all_steps) > 0:
         print()
-        print(all_steps)
+        pprint(all_steps)
+        pprint(workers)
+
+        idle_workers = list(worker for worker in workers if worker.job is None)
+        to_delete = []
+
         for name, step in all_steps.items():
             if len(step.dependencies) > 0:
                 continue
 
+            if not step.worker and len(idle_workers) > 0:
+                step.worker = idle_workers.pop()
+                step.worker.job = step
+                print("starting step", step)
+
+            step.timeLeft -= 1
+
+            if step.timeLeft > 0:
+                continue
+
             print("Ye", name)
+
+            step.worker.job = None
+
             for dep in step.dependants:
                 dep.dependencies.remove(step)
-            order += name
-            del all_steps[name]
-            break
 
-    print(order)
+            to_delete.append(name)
+
+        for name in to_delete:
+            del all_steps[name]
+
+        time += 1
+
+    print(time)
