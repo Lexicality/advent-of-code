d721af6178ffec4d3b2eb0b44324735668bbadde
Lex Robinson <lex.robinson@ly.st>
2020-12-11 12:05:20 +0000

    2020 10.2 brute force attempt (rip)
    Needs more threads

--- src/aoc/y2020/day10.py
+++ src/aoc/y2020/day10.py
@@ -1,18 +1,40 @@
-from collections import defaultdict
-from typing import DefaultDict, Iterable
+import itertools
+from typing import Iterable, List
+
+
+def permutaturize(all_nums: List[int], current: List[int]) -> List[List[int]]:
+    latest = current[-1]
+    assert latest != all_nums[-1]
+
+    i = all_nums.index(latest)
+
+    ret = []
+    for optn in itertools.takewhile(lambda a: a <= latest + 3, all_nums[i + 1 :]):
+        permutation = current.copy()
+        permutation.append(optn)
+
+        ret.append(permutation)
+
+    return ret
 
 
 def main(data: Iterable[str]):
     joltrain = sorted(int(line) for line in data)
-    diffs: DefaultDict[int] = defaultdict(lambda: 0)
 
     joltrain.insert(0, 0)
+    LAST = joltrain[-1] + 3
+    joltrain.append(LAST)
 
-    for i in range(len(joltrain) - 1):
-        diff = abs(joltrain[i] - joltrain[i + 1])
-        diffs[diff] += 1
+    permutations: List[List[int]] = [[0]]
+    complete: List[List[int]] = []
 
-    diffs[3] += 1
+    while len(permutations) > 0:
+        current = permutations.pop(0)
+        results = permutaturize(joltrain, current)
+        for result in results:
+            if result[-1] == LAST:
+                complete.append(result)
+            else:
+                permutations.append(result)
 
-    print(diffs)
-    print(diffs[1] * diffs[3])
+    print(len(complete))
