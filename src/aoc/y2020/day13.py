92f0169c2b23fed4efb0447254f5b1cb9b302055
Lex Robinson <lex@lex.me.uk>
2020-12-14 11:49:27 +0000

    2020 13.2 slightly smarter brute force

--- src/aoc/y2020/day13.py
+++ src/aoc/y2020/day13.py
@@ -16,13 +16,18 @@ def bus_party(timestamp: int, busses: List[Union[int, Literal["x"]]]) -> bool:
 def main(data: Iterable[str]):
     _, schedule = data
     busses = [int(bus) if bus != "x" else "x" for bus in schedule.split(",")]
+    print(len(busses))
+    print(busses[busses[0]])
 
-    first_bus = busses.pop(0)
-    assert isinstance(first_bus, int)
+    first_prime = busses.pop(0)
+    assert isinstance(first_prime, int)
+    second_prime = busses[first_prime - 1]
+    assert isinstance(first_prime, int)
+    itr_value = first_prime * second_prime
 
-    timestamp = 0
+    timestamp = -first_prime
     while True:
-        timestamp += first_bus
+        timestamp += itr_value
         if bus_party(timestamp, busses):
             print(timestamp)
             return
