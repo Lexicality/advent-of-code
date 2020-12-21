66f8cc7556e1b3341f99f22219fe177d465b5479
Lex Robinson <lex@lex.me.uk>
2020-12-21 15:40:07 +0000

    2020 18.2
    I couldn't be bothered to be smart about this one

--- src/aoc/y2020/day18.py
+++ src/aoc/y2020/day18.py
@@ -1,104 +1,59 @@
-from abc import ABC
-from typing import TYPE_CHECKING, Iterable, Literal, Optional, Union
+import re
+from typing import Iterable
 
-from more_itertools import peekable
-
-OPERATORS = {"(", "*", "+", ")"}
-Operator = Union[Literal["*"], Literal["+"]]
-Expr = Union[str, int]
-if TYPE_CHECKING:
-    Exprs = peekable[Expr]
-else:
-    Exprs = peekable
+RE_PARENS = re.compile(r"\(([^())]+)\)")
+RE_ADD = re.compile(r"(\d+) (\+) (\d+)")
+RE_MUL = re.compile(r"(\d+) (\*) (\d+)")
 
 
-class Step(ABC):
-    value: int
+def perform_operation(match: re.Match):
+    n1 = int(match[1])
+    n2 = int(match[3])
+    opr = match[2]
+    if opr == "*":
+        return str(n1 * n2)
+    else:
+        return str(n1 + n2)
 
 
-class Value(Step):
-    def __init__(self, value: int) -> None:
-        self.value = value
+def process_group(group: str) -> str:
+    # print("Processing group", group)
 
-    def __repr__(self) -> str:
-        return f"{self.value}"
+    while "+" in group:
+        new_group = RE_ADD.sub(perform_operation, group, count=1)
+        assert new_group != group, "didn't work :("
+        group = new_group
+        # print("ADDITION!", group)
 
+    while "*" in group:
+        new_group = RE_MUL.sub(perform_operation, group, count=1)
+        assert new_group != group, "didn't work :("
+        group = new_group
+        # print("Multiplication!", group)
 
-class CompilationUnit(Step):
-    left: Step
-    right: Step
-    opr: Operator
-
-    def __init__(self, left: Step, right: Step, operator: Operator) -> None:
-        self.left = left
-        self.right = right
-        self.opr = operator
-
-    @property
-    def value(self) -> int:
-        lv = self.left.value
-        rv = self.right.value
-        if self.opr == "+":
-            return lv + rv
-        elif self.opr == "*":
-            return lv * rv
-        assert False, "??"
-
-    def __repr__(self) -> str:
-        return f"({self.left} {self.opr} {self.right})"
-
-
-def parse_single(exprs: Exprs) -> Step:
-    expr: Expr = next(exprs)
-    if isinstance(expr, int):
-        return Value(expr)
-    assert expr == "("
-    step = None
-    while exprs.peek() != ")":
-        step = compilate(step, exprs)
-    # Consume the ")"
-    next(exprs)
-    return step
-
-
-def compilate(left: Optional[Step], exprs: Exprs) -> Step:
-    if left is None:
-        left = parse_single(exprs)
-
-    opr: Operator = next(exprs)
-    assert opr == "+" or opr == "*", f"Expected operator, got {opr}"
-
-    right = parse_single(exprs)
-
-    return CompilationUnit(left, right, opr)
-
-
-def expressionify(line: str) -> Exprs:
-    for char in list(line):
-        if char == " ":
-            continue
-        elif char in OPERATORS:
-            yield char
-        else:
-            # All the numbers in the input are single digit so we can cheat like crazy
-            yield int(char)
+    return group
 
 
 def do_expr(line: str) -> int:
-    line = "(" + line + ")"
-    exprs = peekable(expressionify(line))
-    unit = parse_single(exprs)
     print("Input:", line)
-    print("Parsed:", unit)
-    print("Output:", unit.value)
-    return unit.value
+    while "(" in line:
+        line = RE_PARENS.sub(lambda m: process_group(m[1]), line, count=1)
+        # print("Grouped!", line)
+
+    line = process_group(line)
+    print("Output:", line)
+    return int(line)
 
 
 def main(input: Iterable[str]):
+    # EXAMPLE_VALUES = [10, 231, 51, 46, 1445, 669060, 23340]
+
     totes = 0
     for line in input:
-        totes += do_expr(line)
-    # ret = do_expr("1 + (2 * 3) + (4 * (5 + 6))")
-    # if ret != 51:
-    #     print("HECK")
+        ans = do_expr(line)
+        # expected = EXAMPLE_VALUES.pop(0)
+        # if ans != expected:
+        #     print("HECK, expected", expected)
+        #     return
+        totes += ans
     print(totes)
