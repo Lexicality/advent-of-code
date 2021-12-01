d789db842a5744041ceb3db210b7cd9d1b496773
Lex Robinson <lex.robinson@ly.st>
2021-12-01 09:57:06 +0000

    2021 1.2

--- src/aoc/y2021/day1.py
+++ src/aoc/y2021/day1.py
@@ -1,15 +1,32 @@
-from typing import Iterable
+from collections import defaultdict
+from typing import DefaultDict, Iterable
+
+
+def _windows(index: int, max: int) -> Iterable[int]:
+    for i in range(index - 2, index + 1):
+        if i >= 0 and i < max:
+            yield i
 
 
 def main(data: Iterable[str]) -> None:
     nums = [int(line) for line in data]
+    max_window = len(nums) - 2
+
+    windows: DefaultDict[int, int] = defaultdict(int)
+    for i, num in enumerate(nums):
+        print(i, num, end=" ")
+        for win in _windows(i, max_window):
+            windows[win] += num
+            print(win, end=" ")
+        print()
 
     sinking = 0
-    last = nums[0]
+    last = windows[0]
 
-    for num in nums:
+    for num in windows.values():
         if num > last:
             sinking += 1
         last = num
+        print(num)
 
     print(sinking)
