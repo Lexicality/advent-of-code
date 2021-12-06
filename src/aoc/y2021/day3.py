1cf7b757992dba2364e36a06a15c76c1d1091456
Lex Robinson <lex@lex.me.uk>
2021-12-06 13:07:13 +0000

    2021 3.2

--- src/aoc/y2021/day3.py
+++ src/aoc/y2021/day3.py
@@ -3,17 +3,57 @@ from typing import Iterable
 import pandas as pd
 
 
+def _extracto(df: pd.DataFrame, mode: bool) -> int:
+    df = df.copy()
+
+    for col in df.columns:
+        counts = df[col].value_counts()
+
+        n_1 = counts["1"]
+        n_0 = counts["0"]
+
+        print()
+        print(df)
+        print(f"Col #{col} 1s: {n_1} 0s: {n_0}")
+
+        if mode:
+            target = "1"
+            if n_1 > n_0:
+                target = "1"
+            elif n_1 < n_0:
+                target = "0"
+        else:
+            target = "0"
+            if n_1 > n_0:
+                target = "0"
+            elif n_1 < n_0:
+                target = "1"
+
+        df = df.loc[df[col] == target]
+
+        print(df)
+
+        if len(df.index) == 1:
+            break
+
+    print("success!")
+    print(df)
+
+    splat = df.agg("".join, axis=1).iloc[0]
+    print(splat)
+
+    return int(splat, base=2)
+
+
 def main(data: Iterable[str]) -> None:
     df = pd.DataFrame([list(line) for line in data])
 
-    df_mode = df.mode(0)
+    print("o2")
+    o2 = _extracto(df, True)
+    print("co2")
+    co2 = _extracto(df, False)
 
-    gamma = int(df_mode.agg("".join, axis=1)[0], base=2)
-    print("gamma", gamma, f"{gamma:b}")
+    print(f"o2 {o2} {o2:b}")
+    print(f"co2 {co2} {co2:b}")
 
-    mask = (2 ** len(df.columns)) - 1
-
-    epsilon = gamma ^ mask
-    print("epsilon", epsilon, f"{epsilon:b}")
-
-    print(gamma * epsilon)
+    print(o2 * co2)
