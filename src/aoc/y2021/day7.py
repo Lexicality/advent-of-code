64b0456b36343a873aa09e1d76eb9e2d5527900d
Lex Robinson <lex@lex.me.uk>
2021-12-08 23:15:49 +0000

    2021 7.2

--- src/aoc/y2021/day7.py
+++ src/aoc/y2021/day7.py
@@ -3,11 +3,21 @@ from typing import Iterable
 import pandas as pd
 
 
+def fuelify(n: int) -> int:
+    return (n * (n + 1)) / 2
+
+
 def main(data: Iterable[str]) -> None:
     crabpos = pd.Series([int(num) for num in next(data).split(",")])
 
-    median = crabpos.median()
+    # brute force because while I got almost the correct mean, I apparently
+    # didn't which is extremely upsetting
+    min_fuel = 91257681  # my incorrect answer
+    for i in range(crabpos.min(), crabpos.max() + 1):
+        fuel = int((crabpos - i).abs().apply(fuelify).sum())
+        min_fuel = min(fuel, min_fuel)
+        if fuel == 91257582:
+            print(i)
+    print(crabpos.mean())
 
-    fuel = (crabpos - median).abs()
-
-    print(fuel.sum())
+    print(min_fuel)
