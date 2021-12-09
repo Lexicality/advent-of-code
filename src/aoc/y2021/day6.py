b1ff57a8a6e7881a83795e7de35aec0b31b32b23
Lex Robinson <lex@lex.me.uk>
2021-12-09 20:02:59 +0000

    2021 6.2 (for real this time)

--- src/aoc/y2021/day6.py
+++ src/aoc/y2021/day6.py
@@ -6,16 +6,25 @@ SIMULATION_DAYS = 256
 
 
 def main(data: Iterable[str]) -> None:
-    fische = pd.Series([int(num) for num in next(data).split(",")])
+    fische = pd.DataFrame([[i, 0] for i in range(9)], columns=("gen", "count"))
+
+    for gen in next(data).split(","):
+        fische.loc[fische["gen"] == int(gen), "count"] += 1
+
+    # print(fische.T.to_string(header=False))
 
     # Love exponential growth
     for i in range(SIMULATION_DAYS):
-        zeros = (fische == 0).sum()
-        if zeros > 0:
-            fische.replace(0, 7, inplace=True)
-            fische = fische.append(pd.Series([9 for _ in range(zeros)]))
-        fische -= 1
-        print(i, end=" ", flush=True)
-        # print(fische.to_frame().T.to_string(header=False, index=False))
+        zeros = fische.loc[fische["gen"] == 0]
+        if not zeros.empty:
+            num_zeros = zeros["count"].iloc[0]
+            fische.loc[fische["gen"] == 0, "gen"] = 9
+            fische.loc[fische["gen"] == 7, "count"] += num_zeros
 
-    print(len(fische))
+        fische["gen"] -= 1
+        print(i, end=" ", flush=True)
+        # print(i)
+        # print(fische.T.to_string(header=False))
+
+    print()
+    print(fische["count"].sum())
