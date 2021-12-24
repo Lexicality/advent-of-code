00bf83514c4b8fb057502d7e8ddaa908e95a9603
Lex Robinson <lex@lex.me.uk>
2021-12-24 11:36:26 +0000

    2021 14.2
    I basically had to be told how to do this by @davidszotten but at least
    it's done now

--- src/aoc/y2021/day14.py
+++ src/aoc/y2021/day14.py
@@ -1,49 +1,55 @@
-from typing import Dict, Iterable, Iterator, List, Tuple
+from collections import defaultdict
+from pprint import pprint
+from typing import Dict, Iterator, Tuple
 
-import numpy as np
+InsertionRules = Dict[str, Tuple[str, Tuple[str, str]]]
+Polymer = Dict[str, int]
 
-Pair = Tuple[int, int]
-InsertionRules = Dict[Pair, int]
-
-NUM_STEPS = 10
-
-
-# strings are expensive and this is exponential
-def destrify(string: str) -> Iterable[int]:
-    for char in string:
-        yield ord(char)
-
-
-def restrify(polymer: Iterable[int]) -> str:
-    return "".join(chr(i) for i in polymer)
+NUM_STEPS = 40
 
 
 def main(data: Iterator[str]) -> None:
-    polymer = np.fromiter(destrify(next(data)), np.int8)
+    counts: Dict[str, int] = defaultdict(int)
+
+    polymer: Polymer = defaultdict(int)
+    polymer_str = next(data)
+
+    for char in polymer_str:
+        counts[char] += 1
+
+    for i in range(len(polymer_str) - 1):
+        polymer[polymer_str[i] + polymer_str[i + 1]] += 1
+
+    pprint(counts)
+    pprint(polymer)
+
+    next(data)  # blank line
 
     rules: InsertionRules = {}
 
-    next(data)  # blank line
     for rulestr in data:
         pair, result = rulestr.split(" -> ")
-        rules[tuple(destrify(pair))] = ord(result)
+        a, b = pair
+        rules[pair] = (result, (a + result, result + b))
 
-    print(restrify(polymer))
+    # pprint(rules)
 
     for i in range(NUM_STEPS):
-        indexs: List[int] = []
-        values: List[int] = []
-        for j in range(len(polymer) - 1):
-            k = j + 1
-            res = rules.get((polymer[j], polymer[k]))
-            if res:
-                indexs.append(k)
-                values.append(res)
-        polymer = np.insert(polymer, indexs, values)
+        new_polymer: Polymer = defaultdict(int)
 
-        # print(f"After step {i+ 1}:", restrify(polymer))
-        print(f"After step {i+ 1}:", len(polymer))
+        for pair, count in polymer.items():
+            letter, (p1, p2) = rules[pair]
+            counts[letter] += count
+            new_polymer[p1] += count
+            new_polymer[p2] += count
 
-    _, counts = np.unique(polymer, return_counts=True)
+        print("Step", i + 1)
+        # pprint(counts)
+        # pprint(new_polymer)
+        # print()
+        polymer = new_polymer
 
-    print(max(counts) - min(counts))
+    pprint(counts)
+    pprint(polymer)
+
+    print(max(counts.values()) - min(counts.values()))
