4ad941e5c1a921da03dc38386f32100d31fb19e4
Lexi Robinson <lexi@lexi.org.uk>
2022-12-04 22:35:11 +0000

    2022 01.1 (rust)

---
+++ src/aoc/y2022/day1.rs
@@ -0,0 +1,27 @@
+pub fn main(data: &mut dyn Iterator<Item = String>) -> String {
+    let mut elves = vec![];
+    let mut running_elf = 0;
+    for line in data {
+        let line = line.trim();
+        if line.is_empty() {
+            elves.push(running_elf);
+            running_elf = 0;
+            continue;
+        }
+        running_elf += line.parse::<i32>().unwrap();
+    }
+    if running_elf > 0 {
+        elves.push(running_elf);
+    }
+
+    elves.sort_unstable();
+    elves.reverse();
+
+    return format!("{}", elves[0]);
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2022",
+    day: "1",
+    func: main,
+});
