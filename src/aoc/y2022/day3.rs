5e5a4520551491c1e21a234854cc6d8b4538ce5e
Lexi Robinson <lexi@lexi.org.uk>
2022-12-07 14:00:48 +0000

    2022 03.2

--- src/aoc/y2022/day3.rs
+++ src/aoc/y2022/day3.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 
 type Pocket = HashSet<char>;
@@ -13,25 +14,32 @@ fn get_priority(item: char) -> u32 {
     return item as u32 - ('a' as u32 - 1);
 }
 
+fn get_comomn_item<L: Iterator<Item = String>>(lines: L) -> Option<char> {
+    let (e1, e2, e3) = lines.collect_tuple()?;
+    let e1: Pocket = e1.chars().collect();
+    let e2: Pocket = e2.chars().collect();
+    let e3: Pocket = e3.chars().collect();
+
+    let wat: Pocket = e1.intersection(&e2).map(|c| *c).collect();
+
+    let intersection: Vec<_> = e3.intersection(&wat).collect();
+
+    if intersection.len() != 1 {
+        panic!("Got multiple intersections: {:?}", intersection)
+    }
+    return Some(*intersection[0]);
+}
+
 pub fn main(data: &mut dyn Iterator<Item = String>) -> String {
     let mut total_prio = 0;
-    for line in data {
-        let len = line.len();
-        if len % 2 != 0 {
-            panic!("Line {} is uneven!", line);
+    for lines in &data.chunks(3) {
+        if let Some(common_item) = get_comomn_item(lines) {
+            let prio = get_priority(common_item);
+            println!("{}/{}", common_item, prio);
+            total_prio += prio;
+        } else {
+            panic!("sad");
         }
-        let mid = len / 2;
-        let (contents1, contents2) = line.split_at(mid);
-        let pocket1: Pocket = contents1.chars().collect();
-        let pocket2: Pocket = contents2.chars().collect();
-        let intersection: Vec<_> = pocket1.intersection(&pocket2).collect();
-        if intersection.len() != 1 {
-            panic!("Got multiple intersections: {:?}", intersection)
-        }
-        let common_item = *intersection[0];
-        let prio = get_priority(common_item);
-        println!("{}: {}/{}", line, common_item, prio);
-        total_prio += prio;
     }
     format!("{}", total_prio)
 }
