36b19eaaa08cdeb3c3fdb7f8637ee6cec321ce61
Lexi Robinson <lexi@lexi.org.uk>
2022-12-07 15:13:24 +0000

    2022 04.1

---
+++ src/aoc/y2022/day4.rs
@@ -0,0 +1,36 @@
+struct Range {
+    start: u32,
+    end: u32,
+}
+impl Range {
+    fn new(input: &str) -> Range {
+        let (start, end) = input.split_once('-').unwrap();
+        Range {
+            start: start.parse().unwrap(),
+            end: end.parse().unwrap(),
+        }
+    }
+
+    fn contains(&self, other: &Range) -> bool {
+        other.start >= self.start && other.end <= self.end
+    }
+}
+
+pub fn main(data: &mut dyn Iterator<Item = String>) -> String {
+    let mut overlap = 0;
+    for pair in data {
+        let (elf1, elf2) = pair.split_once(',').unwrap();
+        let elf1 = Range::new(elf1);
+        let elf2 = Range::new(elf2);
+        if elf1.contains(&elf2) || elf2.contains(&elf1) {
+            overlap += 1;
+        }
+    }
+    format!("{}", overlap)
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2022",
+    day: "4",
+    func: main,
+});
