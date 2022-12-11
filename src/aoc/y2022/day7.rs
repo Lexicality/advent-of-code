c404cf2387fd901568e76854b2121b989dbcd38d
Lex Robinson <lex@lex.me.uk>
2022-12-11 15:48:12 +0000

    2022 07.1
    This was extremely painful and I suspect I did it incorrectly :/

---
+++ src/aoc/y2022/day7.rs
@@ -0,0 +1,205 @@
+use std::borrow::Borrow;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::rc::{Rc, Weak};
+
+type INoder = RefCell<INode>;
+
+enum INode {
+    File {
+        name: String,
+        size: u32,
+        parent: Weak<INoder>,
+    },
+    Folder {
+        name: String,
+        this: Weak<INoder>,
+        children: Vec<Rc<INoder>>,
+        parent: Weak<INoder>,
+    },
+}
+
+impl INode {
+    fn name(&self) -> &str {
+        match self {
+            Self::File { name, .. } => name,
+            Self::Folder { name, .. } => name,
+        }
+    }
+    fn parent(&self) -> &Weak<INoder> {
+        match self {
+            Self::File { parent, .. } => parent,
+            Self::Folder { parent, .. } => parent,
+        }
+    }
+
+    fn size(&self) -> u32 {
+        match self {
+            Self::File { size, .. } => *size,
+            Self::Folder { children, .. } => children
+                .iter()
+                .map(|c| Borrow::<INoder>::borrow(c).borrow().size())
+                .sum(),
+        }
+    }
+
+    fn new_folder(name: String, parent: Weak<INoder>) -> Rc<INoder> {
+        Rc::new_cyclic(move |this| {
+            RefCell::new(Self::Folder {
+                name,
+                this: this.clone(),
+                parent,
+                children: Vec::new(),
+            })
+        })
+    }
+
+    fn add_file(&mut self, name: String, size: u32) {
+        let (this, children) = match self {
+            Self::File { .. } => panic!("I'm a file!"),
+            Self::Folder { this, children, .. } => (this, children),
+        };
+        children.push(Rc::new(RefCell::new(Self::File {
+            name,
+            size,
+            parent: this.clone(),
+        })));
+    }
+
+    fn add_folder(&mut self, name: String) -> Rc<INoder> {
+        let (this, children) = match self {
+            Self::File { .. } => panic!("I'm a file!"),
+            Self::Folder { this, children, .. } => (this, children),
+        };
+        let folder = Self::new_folder(name, this.clone());
+        children.push(folder.clone());
+        return folder;
+    }
+
+    fn get_child(&self, name: &str) -> Rc<INoder> {
+        let children = match self {
+            Self::File { .. } => panic!("I'm a file!"),
+            Self::Folder { children, .. } => children,
+        };
+        return children
+            .iter()
+            // .filter(|c| Borrow::<INoder>::borrow(c).borrow().name() == name);
+            .find(|c| Borrow::<INoder>::borrow(*c).borrow().name() == name)
+            .unwrap()
+            .clone();
+    }
+
+    fn depth(&self) -> usize {
+        let mut parent = self.parent().clone();
+        let mut depth = 0;
+        while let Some(gp) = parent.upgrade() {
+            depth += 1;
+            parent = Borrow::<INoder>::borrow(&gp).borrow().parent().clone();
+        }
+        return depth;
+    }
+}
+
+impl Display for INode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "{:width$}{} {} ({})",
+            "",
+            match self {
+                INode::File { .. } => "- ",
+                INode::Folder { .. } => "/ ",
+            },
+            self.name(),
+            self.size(),
+            width = self.depth()
+        )?;
+        if let INode::Folder { children, .. } = self {
+            for child in children {
+                write!(f, "{}", Borrow::<INoder>::borrow(child).borrow())?;
+            }
+        }
+        return Ok(());
+    }
+}
+
+pub fn main(data: &mut dyn Iterator<Item = String>) -> String {
+    let root = INode::new_folder("/".to_string(), Weak::new());
+    // let mut folders: HashMap<String, Rc<INoder>> = HashMap::new();
+    let mut folders: Vec<Rc<INoder>> = Vec::new();
+    folders.push(root.clone());
+    let mut active_folder = root.clone();
+
+    for line in data.skip(1) {
+        // println!(
+        //     "Current:\n{}line: '{}'",
+        //     Borrow::<INoder>::borrow(&root).borrow(),
+        //     line
+        // );
+        println!("{}", line);
+        if line.starts_with('$') {
+            // it's a command
+            if line == "$ ls" {
+                continue;
+            } else if line.starts_with("$ cd") {
+                let dir = &line[5..];
+                println!("Changing to '{}'", dir);
+                active_folder = if dir == ".." {
+                    Borrow::<INoder>::borrow(&active_folder)
+                        .borrow()
+                        .parent()
+                        .upgrade()
+                        .unwrap()
+                } else {
+                    Borrow::<INoder>::borrow(&active_folder)
+                        .borrow()
+                        .get_child(dir)
+                };
+            }
+        } else {
+            let (size, name) = line.split_once(' ').unwrap();
+            if size == "dir" {
+                // it's a dir
+                println!(
+                    "Adding directory '{}' to {}",
+                    name,
+                    active_folder.borrow_mut().name()
+                );
+                let f = active_folder.borrow_mut().add_folder(name.to_string());
+                folders.push(f);
+            } else {
+                // it's a file!
+                println!(
+                    "Adding file '{}' ({}) to {}",
+                    name,
+                    size,
+                    active_folder.borrow_mut().name()
+                );
+                active_folder
+                    .borrow_mut()
+                    .add_file(name.to_string(), size.parse().unwrap())
+            }
+        }
+    }
+    println!("{}", Borrow::<INoder>::borrow(&root).borrow());
+
+    let size: u64 = folders
+        .iter()
+        .map(|d| {
+            let dir = Borrow::<INoder>::borrow(d).borrow();
+            let size = dir.size();
+            println!("Folder {} is {}", dir.name(), size);
+            size as u64
+        })
+        .filter(|s| s < &100_000)
+        .sum();
+
+    return format!("{}", size);
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2022",
+    day: "7",
+    func: main,
+});
