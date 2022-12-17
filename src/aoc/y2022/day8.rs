3876010b93c7822c2ccfbe03941f7c1730aece73
Lexi Robinson <lexi@lexi.org.uk>
2022-12-17 17:09:06 +0000

    2022 08.2

--- src/aoc/y2022/day8.rs
+++ src/aoc/y2022/day8.rs
@@ -5,10 +5,10 @@ use ansi_term::{Color, Style};
 use lazy_static::lazy_static;
 
 const NUM_SIZES: usize = 10;
-const NUM_COLOURS: u8 = 24;
+const BASE_COLOUR: u8 = 238;
+const NUM_COLOURS: u8 = 255 - BASE_COLOUR + 1;
 const COLOUR_GAP: f64 = NUM_COLOURS as f64 / (NUM_SIZES - 1) as f64;
-const BASE_COLOUR: u8 = 232;
-const COLOURS: [u8; NUM_SIZES] = [
+const _COLOURS: [u8; NUM_SIZES] = [
     BASE_COLOUR,
     BASE_COLOUR + (COLOUR_GAP * 1.0) as u8,
     BASE_COLOUR + (COLOUR_GAP * 2.0) as u8,
@@ -21,33 +21,7 @@ const COLOURS: [u8; NUM_SIZES] = [
     255,
 ];
 lazy_static! {
-    static ref HIDDEN_COLOURS: [Style; NUM_SIZES] =
-        COLOURS.map(|c| Color::Fixed(c).on(Color::Green));
-    static ref VISIBLE_COLOURS: [Style; NUM_SIZES] =
-        COLOURS.map(|c| Color::Fixed(c).on(Color::Red));
-    static ref MYSTERY_COLOURS: [Style; NUM_SIZES] =
-        COLOURS.map(|c| Color::Fixed(c).on(Color::Cyan));
-}
-
-enum Visibility {
-    FileNotFound,
-    Visible,
-    Hidden,
-}
-impl Visibility {
-    fn setup(val: bool) -> Visibility {
-        match val {
-            true => Visibility::Visible,
-            false => Visibility::FileNotFound,
-        }
-    }
-
-    fn done(&self) -> bool {
-        match self {
-            Visibility::FileNotFound => false,
-            _ => true,
-        }
-    }
+    static ref COLOURS: [Style; NUM_SIZES] = _COLOURS.map(|c| Color::Fixed(c).on(Color::Black));
 }
 
 #[derive(Debug, Clone)]
@@ -59,15 +33,6 @@ enum Direction {
 }
 
 impl Direction {
-    fn rev(&self) -> Direction {
-        match self {
-            Direction::NORTH => Direction::SOUTH,
-            Direction::EAST => Direction::WEST,
-            Direction::SOUTH => Direction::NORTH,
-            Direction::WEST => Direction::EAST,
-        }
-    }
-
     fn idx(&self) -> usize {
         match self {
             Direction::NORTH => 0,
@@ -83,57 +48,28 @@ type Coord = (u32, u32);
 struct Tree {
     // coord: Coord,
     height: u32,
-    total_visibility: Visibility,
-    cardinal_visibility: [Visibility; 4],
+    cardinal_visibility: [usize; 4],
 }
 
 impl Tree {
-    fn new(size: usize, coord: Coord, height: u32) -> Tree {
-        let last = (size - 1) as u32;
-        let (x, y) = coord;
-        let north = x == 0;
-        let east = y == last;
-        let south = x == last;
-        let west = y == 0;
-
+    fn new(height: u32) -> Tree {
         Tree {
             // coord,
             height,
-            total_visibility: Visibility::setup(north || east || south || west),
-            cardinal_visibility: [
-                Visibility::setup(north),
-                Visibility::setup(east),
-                Visibility::setup(south),
-                Visibility::setup(west),
-            ],
+            cardinal_visibility: [0, 0, 0, 0],
         }
     }
 
     fn style(&self) -> &'static Style {
-        let colours: &[Style; NUM_SIZES] = match self.total_visibility {
-            Visibility::FileNotFound => &MYSTERY_COLOURS,
-            Visibility::Hidden => &HIDDEN_COLOURS,
-            Visibility::Visible => &VISIBLE_COLOURS,
-        };
-        return &colours[self.height as usize];
+        return &COLOURS[self.height as usize];
     }
 
-    fn done(&self, direction: &Direction) -> bool {
-        self.total_visibility.done() || self.cardinal_visibility[direction.idx()].done()
-    }
-
-    fn set_vis(&mut self, direction: &Direction, visibility: Visibility) {
-        if let Visibility::Visible = visibility {
-            self.total_visibility = Visibility::Visible;
-        }
+    fn set_vis(&mut self, direction: &Direction, visibility: usize) {
         self.cardinal_visibility[direction.idx()] = visibility;
     }
 
-    fn is_visible(&self) -> bool {
-        match self.total_visibility {
-            Visibility::Visible => true,
-            _ => false,
-        }
+    fn get_visibility(&self) -> usize {
+        self.cardinal_visibility.iter().product()
     }
 }
 
@@ -152,7 +88,7 @@ impl Forest {
         for line in data {
             for (x, height) in line.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
                 let coord: Coord = (x as u32, y);
-                forest.trees.insert(coord, Tree::new(size, coord, height));
+                forest.trees.insert(coord, Tree::new(height));
             }
             y += 1;
         }
@@ -172,23 +108,15 @@ impl Forest {
         return iter;
     }
 
-    fn check_vis(&self, coord: &Coord, direction: &Direction, tree_height: u32) -> Visibility {
+    fn check_vis(&self, coord: &Coord, direction: &Direction, tree_height: u32) -> usize {
+        let mut vis = 0;
         for o_coord in self.zoop(coord, direction) {
+            vis += 1;
             if self.get_tree(&o_coord).height >= tree_height {
-                return Visibility::Hidden;
+                return vis;
             }
         }
-        return Visibility::Visible;
-    }
-
-    fn invisify(&mut self, coord: &Coord, direction: &Direction) {
-        for o_coord in self.zoop(&coord, &direction.rev()) {
-            self.get_tree_mut(&o_coord).set_vis(
-                // ???
-                direction,
-                Visibility::Hidden,
-            );
-        }
+        return vis;
     }
 
     fn get_tree(&self, coord: &Coord) -> &Tree {
@@ -199,14 +127,8 @@ impl Forest {
     }
 
     fn floodify(&mut self, direction: &Direction, coord: Coord) {
-        if self.get_tree(&coord).done(&direction) {
-            return;
-        }
         let tree_height = self.get_tree(&coord).height;
 
-        if tree_height == 9 {
-            self.invisify(&coord, &direction);
-        }
         let visibility = self.check_vis(&coord, &direction, tree_height);
         self.get_tree_mut(&coord).set_vis(&direction, visibility);
     }
@@ -224,38 +146,41 @@ impl Forest {
                 ] {
                     self.floodify(&dir, coord.clone());
                 }
-                let tree = self.get_tree_mut(&coord);
-                if let Visibility::FileNotFound = tree.total_visibility {
-                    tree.total_visibility = Visibility::Hidden;
-                }
+                // let tree = self.get_tree(&coord);
+                // if tree.get_visibility() > 2 {
+                // println!("{:?}: {:?}", coord, tree.cardinal_visibility);
+                // }
             }
+            // println!("");
         }
     }
 
-    fn count_visible(&self) -> usize {
+    fn get_visibilist(&self) -> usize {
         self.trees
             .values()
-            .map(|tree| tree.is_visible())
-            .filter(|v| *v)
-            .count()
+            .map(|tree| tree.get_visibility())
+            .max()
+            .unwrap()
     }
 }
 
 impl Display for Forest {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let initial_style = Style::new();
-        let mut cstyle = &initial_style;
         for y in 0..self.size {
-            write!(f, "{}", cstyle.prefix())?;
             for x in 0..self.size {
                 let coord = (x as u32, y as u32);
                 let tree = self.trees.get(&coord).unwrap();
                 let style = tree.style();
-                write!(f, "{}{}", cstyle.infix(*style), tree.height)?;
-                cstyle = style;
+                write!(
+                    f,
+                    "{}{}{}({:02})",
+                    style.prefix(),
+                    tree.height,
+                    style.suffix(),
+                    tree.get_visibility()
+                )?;
             }
-            write!(f, "{}\n", cstyle.suffix())?;
-            cstyle = &initial_style;
+            write!(f, "\n")?;
         }
         Ok(())
     }
@@ -267,11 +192,8 @@ pub fn main(data: &mut dyn Iterator<Item = String>) -> String {
     let mut forest = Forest::new(size, &mut data);
     println!("{}", forest);
     forest.flood();
-    // forest.flood_north();
-    // println!("{}", forest);
-    // forest.flood_south();
     println!("{}", forest);
-    return format!("{}", forest.count_visible());
+    return format!("{}", forest.get_visibilist());
 }
 
 inventory::submit!(crate::AoCDay {
