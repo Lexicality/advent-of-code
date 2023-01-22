d51e131d61e67df818be5f1d34d35f2b07047367
Lexi Robinson <lexi@lexi.org.uk>
2023-01-22 20:24:31 +0000

    WIP 2022 14.1
    I probably won't actually finish this one but here's some data.

---
+++ src/aoc/y2022/day14.rs
@@ -0,0 +1,17 @@
+use crate::Coord2D;
+
+pub fn main(data: crate::DataIn) -> String {
+    for line in data {
+        for coord in line.split(" -> ") {
+            let coord = Coord2D::parse(coord);
+            println!("{}", coord);
+        }
+    }
+    format!("")
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2022",
+    day: "14",
+    func: main,
+});
