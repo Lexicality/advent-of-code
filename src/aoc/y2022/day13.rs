2a6ff0355f321317e77f517c79b114e718e2ee1b
Lexi Robinson <lexi@lexi.org.uk>
2023-01-22 18:43:53 +0000

    2022 13.2

--- src/aoc/y2022/day13.rs
+++ src/aoc/y2022/day13.rs
@@ -1,6 +1,7 @@
 use itertools::EitherOrBoth as EoB;
 use itertools::Itertools;
 use std::cmp::Ordering;
+use std::fmt::Display;
 
 use serde_json::{json, Value};
 
@@ -28,12 +29,15 @@ fn actual_check_order(left: &Value, right: &Value) -> Option<Order> {
             _ => panic!("Unexpected value!"),
         },
         Value::Array(left) => {
+            let rvec: Vec<_>;
             let right = match right {
-                Value::Number(right) => json!([right]),
-                Value::Array(_) => right.clone(),
+                Value::Number(_) => {
+                    rvec = vec![right.clone()];
+                    &rvec
+                }
+                Value::Array(right) => right,
                 _ => panic!("Unexpected value!"),
             };
-            let right = right.as_array().unwrap();
             let iter = left.iter().zip_longest(right);
             for item in iter {
                 match item {
@@ -53,27 +57,51 @@ fn actual_check_order(left: &Value, right: &Value) -> Option<Order> {
     }
 }
 
-fn check_order(left: Value, right: Value) -> Order {
-    actual_check_order(&left, &right).unwrap()
+struct Sigh<'a>(&'a Vec<Value>);
+
+impl<'a> Display for Sigh<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in self.0 {
+            line.fmt(f)?;
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 pub fn main(data: crate::DataIn) -> String {
-    let mut index = 0;
-    let mut ret = 0;
-    loop {
-        index += 1;
-        let v1: Value = serde_json::from_str(&data.next().unwrap()).unwrap();
-        let v2: Value = serde_json::from_str(&data.next().unwrap()).unwrap();
-        let order = check_order(v1, v2);
-        if let Order::Right = order {
-            ret += index;
+    let mut signal = Vec::with_capacity(data.size_hint().0 + 2);
+    let first_marker = json!([[2]]);
+    let second_marker = json!([[6]]);
+    signal.push(first_marker.clone());
+    signal.push(second_marker.clone());
+    for line in data {
+        if line.is_empty() {
+            continue;
         }
-
-        if data.next().is_none() {
-            break;
-        }
+        signal.push(serde_json::from_str(&line).unwrap());
     }
-    format!("{}", ret)
+    println!("{}", Sigh(&signal));
+    signal.sort_unstable_by(
+        |left, right| match actual_check_order(left, right).unwrap() {
+            Order::Right => Ordering::Less,
+            Order::Wrong => Ordering::Greater,
+        },
+    );
+    println!("{}", Sigh(&signal));
+    let fpos = signal
+        .iter()
+        .find_position(|v| v == &&first_marker)
+        .map(|d| d.0)
+        .expect("Can't find first marker")
+        + 1;
+    let spos = signal
+        .iter()
+        .find_position(|v| v == &&second_marker)
+        .map(|d| d.0)
+        .expect("Can't find second marker")
+        + 1;
+    format!("{}", fpos * spos)
 }
 
 inventory::submit!(crate::AoCDay {
