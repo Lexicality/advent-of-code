a22668bbd0f0ac089a152bd9707d689945302744
Lexi Robinson <lexi@lexi.org.uk>
2023-10-27 17:43:34 +0100

    2022 09.2

--- src/aoc/y2022/day09.rs
+++ src/aoc/y2022/day09.rs
@@ -54,33 +54,66 @@ fn magic_clamp(val: i32) -> i32 {
     }
 }
 
+impl Coord2D {
+    fn maybe_move(self, parent: Self) -> Option<Self> {
+        let dist = parent - self;
+        if !(dist.x.abs() > 1 || dist.y.abs() > 1) {
+            return None;
+        }
+        if dist.x == 0 || dist.y == 0 {
+            Some(self + (dist / 2).unwrap())
+        } else {
+            Some(
+                self + Coord2D {
+                    x: magic_clamp(dist.x),
+                    y: magic_clamp(dist.y),
+                },
+            )
+        }
+    }
+}
+
 pub fn main(data: crate::DataIn) -> String {
     let mut head_pos = Coord2D { x: 0, y: 0 };
+    let mut midroll: [Coord2D; 8] = [head_pos; 8];
     let mut tail_pos = head_pos;
 
     let mut visited: HashSet<Coord2D> = HashSet::new();
     visited.insert(tail_pos);
 
     for line in data {
+        // println!("== {line} ==");
         let instr: Instruction = line.parse().unwrap();
         for coord in instr.coordinate() {
             head_pos = head_pos + coord;
-            let dist = head_pos - tail_pos;
-            if dist.x.abs() > 1 || dist.y.abs() > 1 {
-                // eyy I move a da tail
-                if dist.x == 0 || dist.y == 0 {
-                    // we can cheat beacuse we know the length is 2
-                    tail_pos = tail_pos + (dist / 2).unwrap();
-                } else {
-                    // fuck idk
-                    tail_pos = tail_pos
-                        + Coord2D {
-                            x: magic_clamp(dist.x),
-                            y: magic_clamp(dist.y),
-                        }
+            let mut last = head_pos;
+            midroll.iter_mut().for_each(|coord| {
+                let beep = coord.maybe_move(last);
+                if let Some(yay) = beep {
+                    coord.x = yay.x;
+                    coord.y = yay.y;
                 }
+                last = *coord;
+            });
+            let maybe_tail = tail_pos.maybe_move(last);
+            if let Some(new_tail) = maybe_tail {
+                tail_pos = new_tail;
                 visited.insert(tail_pos);
             }
+            // let mut grid = Grid::<char>::new_empty(6, 5);
+            // for coord in grid.keys() {
+            //     grid.set(coord, '.');
+            // }
+            // grid.set(tail_pos, '9');
+            // for (coord, name) in midroll
+            //     .iter()
+            //     .zip(['1', '2', '3', '4', '5', '6', '7', '8'])
+            //     .rev()
+            // {
+            //     grid.set(*coord, name);
+            // }
+            // grid.set(head_pos, 'H');
+            // println!("{grid}");
         }
     }
     visited.len().to_string()
