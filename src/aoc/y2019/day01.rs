30f68f19f3c6d9aebf479ad7553e43159c9051f0
Lex Robinson <lex@lex.me.uk>
2023-12-02 11:26:13 +0000

    2019 01.2

--- src/aoc/y2019/day01.rs
+++ src/aoc/y2019/day01.rs
@@ -2,7 +2,15 @@ pub fn main(data: crate::DataIn) -> String {
     let mut ret: u64 = 0;
     for line in data {
         let num: u64 = line.parse().unwrap();
-        let req = (num / 3) - 2;
+        let mut weight = num;
+        let mut req = 0;
+        loop {
+            weight = (weight / 3).saturating_sub(2);
+            if weight == 0 {
+                break;
+            }
+            req += weight;
+        }
         println!("Input: {num} Required: {req}");
         ret += req;
     }
