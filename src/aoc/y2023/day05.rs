df430852c8773dd35f5ef194f8474c8a915e1ca1
Lex Robinson <lex@lex.me.uk>
2023-12-05 17:07:14 +0000

    2023 05.2

--- src/aoc/y2023/day05.rs
+++ src/aoc/y2023/day05.rs
@@ -6,7 +6,43 @@ use regex::Regex;
 
 use crate::{AoCError, AoCResult};
 
+#[derive(Debug)]
+enum WrangleState {
+    Disjoint,
+    MatchesWhole(Range<u64>),
+    MatchesPart(Range<u64>, Vec<Range<u64>>),
+}
+
+#[allow(clippy::single_range_in_vec_init)]
+fn range_wrangle(input: &Range<u64>, range: &Range<u64>) -> WrangleState {
+    let contains_start = range.contains(&input.start);
+    let contains_end = range.contains(&(input.end - 1));
+    if input.is_empty() {
+        unreachable!("input {input:?} is empty");
+    }
+    match (contains_start, contains_end) {
+        (true, true) => WrangleState::MatchesWhole(input.clone()),
+        (false, true) => {
+            WrangleState::MatchesPart(range.start..input.end, vec![input.start..range.start])
+        }
+        (true, false) => {
+            WrangleState::MatchesPart(input.start..range.end, vec![range.end..input.end])
+        }
+        (false, false) => {
+            if input.start < range.start && input.end > range.end {
+                WrangleState::MatchesPart(
+                    range.clone(),
+                    vec![input.start..range.start, range.end..input.end],
+                )
+            } else {
+                WrangleState::Disjoint
+            }
+        }
+    }
+}
+
 #[allow(dead_code)]
+#[derive(Debug)]
 struct Mapping {
     from: String,
     to: String,
@@ -16,7 +52,7 @@ struct Mapping {
 impl Mapping {
     fn parse(data: crate::DataIn) -> AoCResult<Self> {
         lazy_static! {
-            static ref NAME_RE: Regex = Regex::new(r"^(.+)-(.+) map:$").unwrap();
+            static ref NAME_RE: Regex = Regex::new(r"^(.+)-to-(.+) map:$").unwrap();
         }
         let name_line = data.next().ok_or(AoCError::new("Name line is missing"))?;
         let matches = NAME_RE
@@ -36,34 +72,80 @@ impl Mapping {
                 .splitn(3, ' ')
                 .map(|num| num.parse().map_err(AoCError::new_from_parseerror))
                 .collect::<Result<Vec<u64>, _>>()?
-                .drain(..)
+                .into_iter()
                 .collect_tuple()
                 .ok_or_else(|| AoCError::new(format!("Bad range line {line}")))?;
             ret.ranges
                 .push((source..(source + count), dest..(dest + count)));
         }
+        // Sort for easier munging
+        ret.ranges.sort_by_key(|(start, _)| start.start);
+        let mut fillers = Vec::with_capacity(3); // There's only one disjoint per category
+
+        let start = ret.ranges.first().unwrap().0.start;
+        fillers.push(0..start);
+        let end = ret.ranges.last().unwrap().0.end;
+        fillers.push(end..u64::MAX);
+
+        for (a, b) in ret.ranges.iter().map(|(start, _)| start).tuple_windows() {
+            if a.end != b.start {
+                fillers.push(a.end..b.start);
+            }
+        }
+        for filler in fillers {
+            ret.ranges.push((filler.clone(), filler));
+        }
+        // Sort again just for my own sanity
+        ret.ranges.sort_by_key(|(start, _)| start.start);
 
         Ok(ret)
     }
 
-    fn map(&self, value: u64) -> u64 {
-        if let Some((from, to)) = self.ranges.iter().find(|(start, _)| start.contains(&value)) {
-            (value - from.start) + to.start
-        } else {
-            value
+    fn map_ranges(&self, mut ranges: Vec<Range<u64>>) -> Vec<Range<u64>> {
+        let mut output = Vec::with_capacity(ranges.len());
+        while let Some(input) = ranges.pop() {
+            for (start, end) in self.ranges.iter() {
+                let input = match range_wrangle(&input, start) {
+                    WrangleState::MatchesWhole(input) => input,
+                    WrangleState::Disjoint => continue,
+                    WrangleState::MatchesPart(input, remainder) => {
+                        ranges.extend(remainder);
+                        input
+                    }
+                };
+                let offset = input.start - start.start;
+                let size = input.end - input.start;
+                let result = (end.start + offset)..(end.start + offset + size);
+                if result.is_empty() {
+                    panic!("wat input: {input:?} start: {start:?} end: {end:?}\n offset: {offset} size: {size}\n result: {result:?}");
+                }
+                output.push(result);
+                break;
+            }
         }
+        output
     }
+}
+
+pub fn get_seeds(line: &str) -> Vec<Range<u64>> {
+    let mut seeds_iter = line.split(' ');
+    assert_eq!(seeds_iter.next(), Some("seeds:"));
+    let seeds_tmp = seeds_iter
+        .map(|num| num.parse())
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
+    assert_eq!(seeds_tmp.len() % 2, 0, "non-even number of seeds??");
+    let mut ret = Vec::with_capacity(seeds_tmp.len() / 2);
+    let mut seeds_iter = seeds_tmp.into_iter();
+    while let Some((start, count)) = seeds_iter.next_tuple() {
+        ret.push(start..(start + count));
+    }
+    ret
 }
 
 pub fn main(data: crate::DataIn) -> String {
     let mut data = data.peekable();
-    let seeds = data.next().unwrap();
-    let mut seeds = seeds.split(' ');
-    assert_eq!(seeds.next(), Some("seeds:"));
-    let seeds: Vec<u64> = seeds
-        .map(|num| num.parse())
-        .collect::<Result<_, _>>()
-        .unwrap();
+    let seeds = get_seeds(&data.next().unwrap());
     assert!(data.next().unwrap().is_empty());
     // these are in the correct order so I'm not going to bother with name lookups for now
     let mut mappings: Vec<Mapping> = Vec::with_capacity(7);
@@ -72,8 +154,16 @@ pub fn main(data: crate::DataIn) -> String {
     }
 
     let ret = seeds
-        .iter()
-        .map(|seed| mappings.iter().fold(*seed, |val, mapper| mapper.map(val)))
+        .into_iter()
+        .map(|seed| {
+            mappings
+                .iter()
+                .fold(vec![seed], |val, mapper| mapper.map_ranges(val))
+                .into_iter()
+                .map(|range| range.start)
+                .min()
+                .unwrap()
+        })
         .min()
         .unwrap();
 
