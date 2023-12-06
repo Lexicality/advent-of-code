f5d161eee60e2aa823366fdd01cc15473fb10e27
Lexi Robinson <lexi@lexi.org.uk>
2023-12-06 09:50:58 +0000

    2023 06.2

--- src/aoc/y2023/day06.rs
+++ src/aoc/y2023/day06.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 #[allow(unused_variables, unused_mut)]
 pub fn main(data: crate::DataIn) -> String {
     let times = data.next().unwrap();
@@ -6,26 +8,20 @@ pub fn main(data: crate::DataIn) -> String {
     let mut distances = distances.split_whitespace();
     assert_eq!(times.next(), Some("Time:"));
     assert_eq!(distances.next(), Some("Distance:"));
-    let mut ret = 0;
-    for (time, record) in times.zip(distances) {
-        let race_time: u32 = time.parse().unwrap();
-        let record: u32 = record.parse().unwrap();
-        let mut winnables = 0;
-        let mut hump = false;
-        for hold_time in 0..race_time {
-            let time = race_time - hold_time;
-            let distance = hold_time * time;
-            if distance < record && hump {
-                break;
-            } else if distance > record {
-                hump = true;
-                winnables += 1;
-            }
-        }
-        if ret == 0 {
-            ret = winnables;
-        } else {
-            ret *= winnables;
+    let mut ret: u64 = 0;
+    let race_time = times.join("");
+    let record = distances.join("");
+    let race_time: u64 = race_time.parse().unwrap();
+    let record: u64 = record.parse().unwrap();
+    let mut hump = false;
+    for hold_time in 0..race_time {
+        let time = race_time - hold_time;
+        let distance = hold_time * time;
+        if distance < record && hump {
+            break;
+        } else if distance > record {
+            hump = true;
+            ret += 1;
         }
     }
     ret.to_string()
