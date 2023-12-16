40a471d46c889f4780ee8c0324a31c155703657b
Lex Robinson <lex@lex.me.uk>
2023-12-16 21:38:07 +0000

    2023 10.2

--- src/aoc/y2023/day10.rs
+++ src/aoc/y2023/day10.rs
@@ -1,6 +1,8 @@
 use std::collections::{HashSet, VecDeque};
 use std::fmt::Display;
 
+use itertools::Itertools;
+
 use crate::{AoCError, Coord2D, Direction, Grid};
 
 #[derive(Debug)]
@@ -14,7 +16,6 @@ enum PipeSegment {
     BendSouthEast,
     Mystery,
 }
-
 impl TryFrom<char> for PipeSegment {
     type Error = AoCError;
     fn try_from(value: char) -> Result<Self, Self::Error> {
@@ -35,7 +36,7 @@ impl TryFrom<char> for PipeSegment {
 impl Display for PipeSegment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            PipeSegment::Ground => f.fill(),
+            Self::Ground => '░',
             Self::Vertical => '║',
             Self::Horizontal => '═',
             Self::BendNorthEast => '╚',
@@ -43,6 +44,26 @@ impl Display for PipeSegment {
             Self::BendSouthEast => '╔',
             Self::BendSouthWest => '╗',
             Self::Mystery => '╳',
+        }
+        .fmt(f)
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+#[allow(dead_code)]
+enum Pipe2 {
+    #[default]
+    Ground,
+    Pipe,
+    Outside,
+}
+
+impl Display for Pipe2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ground => '░',
+            Self::Outside => '╳',
+            Self::Pipe => '█',
         }
         .fmt(f)
     }
@@ -61,24 +82,24 @@ impl PipeSegment {
             Self::BendNorthWest => directionate(pos, Direction::North, Direction::West),
             Self::BendSouthEast => directionate(pos, Direction::South, Direction::East),
             Self::BendSouthWest => directionate(pos, Direction::South, Direction::West),
-            Self::Ground => None,
-            Self::Mystery => None,
+            _ => None,
         }
     }
 }
 
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
-    let grid: Grid<PipeSegment> = Grid::new_from_lines(data.map(|line| {
+    let mut grid: Grid<PipeSegment> = Grid::new_from_lines(data.map(|line| {
         line.chars()
             .map(|c| c.try_into().unwrap())
             .collect::<Vec<_>>()
     }));
-    println!("{grid}");
+    // println!("{grid}");
+
     let start = grid
         .find(|(_, item)| matches!(item, PipeSegment::Mystery))
         .expect("Must have a starting position");
 
-    let mut queue: VecDeque<(u32, Coord2D)> = grid
+    let mut queue: VecDeque<Coord2D> = grid
         .get_neighbours(start, false)
         .filter(|coord| {
             grid.get(coord)
@@ -86,34 +107,124 @@ pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
                 .get_neighbours(*coord)
                 .is_some_and(|(a, b)| a == start || b == start)
         })
-        .map(|coord| (1, coord))
         .collect();
 
     let mut seen: HashSet<Coord2D> = HashSet::with_capacity(grid.len());
     seen.insert(start);
 
-    let mut ret = 0;
-    while let Some((mut depth, coord)) = queue.pop_front() {
-        ret = ret.max(depth);
+    while let Some(coord) = queue.pop_front() {
         seen.insert(coord);
         let item = grid.get(&coord).unwrap();
         // working on the assumption here that the loop will never go out of bounds
         let (a, b) = item.get_neighbours(coord).expect("loop must be contiguous");
-        depth += 1;
         if !seen.contains(&a) {
-            queue.push_back((depth, a));
+            queue.push_back(a);
         }
         if !seen.contains(&b) {
-            queue.push_back((depth, b));
+            queue.push_back(b);
+        }
+    }
+
+    grid.iter_mut()
+        .filter(|(coord, _)| !seen.contains(coord))
+        .for_each(|(_, item)| *item = PipeSegment::Ground);
+
+    println!("{grid}");
+
+    // Expand the grid
+    let width = grid.width;
+    let height = grid.height;
+
+    let oneone = Coord2D { x: 1, y: 1 };
+    let size = Coord2D { x: 3, y: 3 };
+
+    let mut grid: Grid<Pipe2> = grid
+        .into_iter()
+        .flat_map(|(coord, item)| {
+            let base = coord * size;
+            let mut microgrid = Grid::new_filled(3, 3, Pipe2::Ground);
+            match item {
+                PipeSegment::Ground => (),
+                PipeSegment::Mystery => {
+                    microgrid.set((0, 1).into(), Pipe2::Pipe);
+                    microgrid.set((1, 1).into(), Pipe2::Pipe);
+                    microgrid.set((1, 0).into(), Pipe2::Pipe);
+                    microgrid.set((1, 2).into(), Pipe2::Pipe);
+                    microgrid.set((2, 1).into(), Pipe2::Pipe);
+                }
+                _ => {
+                    let (a, b) = item.get_neighbours(oneone).unwrap();
+                    microgrid.set(oneone, Pipe2::Pipe);
+                    microgrid.set(a, Pipe2::Pipe);
+                    microgrid.set(b, Pipe2::Pipe);
+                }
+            }
+            microgrid
+                .into_iter()
+                .map(|(coord, item)| (coord + base, item))
+                .collect_vec()
+        })
+        .collect();
+
+    println!("{grid}");
+
+    // Flood fill
+    // Conveniently it's always safe to start in the top left because on my input data
+    // the top left is never the starting position
+
+    // Fill seen with the pipes so we'll never iterate into them and so I won't have to bother about filtering
+    let mut seen: HashSet<_> = grid
+        .iter()
+        .filter(|(_, item)| matches!(item, Pipe2::Pipe))
+        .map(|(coord, _)| coord)
+        .copied()
+        .collect();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((0, 0).into());
+    while let Some(coord) = queue.pop_front() {
+        if !seen.insert(coord) {
+            continue;
+        }
+
+        grid.set(coord, Pipe2::Outside);
+        queue.extend(
+            grid.get_neighbours(coord, false)
+                .filter(|coord| !seen.contains(coord)),
+        );
+    }
+
+    println!("{grid}");
+
+    // shrink the grid again
+    let mut ret = 0;
+    for x in 0..width as i32 {
+        for y in 0..height as i32 {
+            let coord = Coord2D::from((x, y)) * size + oneone;
+            if matches!(grid.get(&coord), Some(Pipe2::Ground)) {
+                ret += 1;
+            }
         }
     }
 
     Ok(ret.to_string())
 }
 
+pub fn main_example(data: crate::DataIn) -> crate::AoCResult<String> {
+    loop {
+        let mut crimes = data.take_while(|line| !line.is_empty()).peekable();
+        if crimes.peek().is_none() {
+            break;
+        }
+        let res = main(&mut crimes)?;
+        println!("=== {res} ===\n");
+    }
+    Ok("".to_string())
+}
+
 inventory::submit!(crate::AoCDay {
     year: "2023",
     day: "10",
     func: main,
-    example_func: None,
+    example_func: Some(main_example),
 });
