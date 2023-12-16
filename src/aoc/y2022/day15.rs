a5bb12243bf90aa4f0bc2985dc6354f255f36152
Lex Robinson <lex@lex.me.uk>
2023-12-16 14:01:52 +0000

    Get rid of 2022.15's custom coordinate class
    rust-analyzer is being annoying about suggesting importing it

--- src/aoc/y2022/day15.rs
+++ src/aoc/y2022/day15.rs
@@ -3,55 +3,12 @@ use std::cmp;
 use lazy_static::lazy_static;
 use regex::Regex;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default)]
-pub struct Coord2D {
-    pub x: i64,
-    pub y: i64,
-}
-
-impl Coord2D {
-    pub const MAX: Coord2D = Coord2D {
-        x: i64::MAX,
-        y: i64::MAX,
-    };
-    pub const MIN: Coord2D = Coord2D {
-        x: i64::MIN,
-        y: i64::MIN,
-    };
-
-    pub fn parse(data: &str) -> Coord2D {
-        let (x, y) = data
-            .split_once(',')
-            .expect("Coordinates must be in the form x,y");
-        Coord2D {
-            x: x.parse().expect("Failed to parse X:"),
-            y: y.parse().expect("Failed to parse Y:"),
-        }
-    }
-
-    pub fn distance(&self, other: &Coord2D) -> u64 {
-        (self.x - other.x).unsigned_abs() + (self.y - other.y).unsigned_abs()
-    }
-
-    pub fn get_max(&self, other: &Coord2D) -> Coord2D {
-        Coord2D {
-            x: cmp::max(self.x, other.x),
-            y: cmp::max(self.y, other.y),
-        }
-    }
-
-    pub fn get_min(&self, other: &Coord2D) -> Coord2D {
-        Coord2D {
-            x: cmp::min(self.x, other.x),
-            y: cmp::min(self.y, other.y),
-        }
-    }
-}
+use crate::{utils::bigcoord2d::BigCoord2D, Coordinate};
 
 #[derive(Debug)]
 struct Reading {
-    sensor: Coord2D,
-    beacon: Coord2D,
+    sensor: BigCoord2D,
+    beacon: BigCoord2D,
     distance: u64,
 }
 
@@ -64,11 +21,11 @@ impl Reading {
             .unwrap();
         }
         let matches = RE.captures(&line).expect("Line should match regex?");
-        let sensor = Coord2D {
+        let sensor = BigCoord2D {
             x: matches[1].parse().unwrap(),
             y: matches[2].parse().unwrap(),
         };
-        let beacon = Coord2D {
+        let beacon = BigCoord2D {
             x: matches[3].parse().unwrap(),
             y: matches[4].parse().unwrap(),
         };
@@ -83,7 +40,7 @@ impl Reading {
 pub fn main(data: crate::DataIn) -> String {
     let readings: Vec<Reading> = data.map(Reading::parse).collect();
     let (min, max, maxdist) = readings.iter().fold(
-        (Coord2D::MAX, Coord2D::MIN, 0),
+        (BigCoord2D::MAX, BigCoord2D::MIN, 0),
         |(min, max, maxdist), reading| {
             (
                 min.get_min(&reading.sensor).get_min(&reading.beacon),
@@ -97,7 +54,7 @@ pub fn main(data: crate::DataIn) -> String {
 
     let count = (min.x - maxdist..=max.x + maxdist)
         .filter(|x| {
-            let coord = Coord2D { x: *x, y };
+            let coord = BigCoord2D { x: *x, y };
             readings.iter().any(|reading| {
                 reading.sensor.distance(&coord) <= reading.distance && coord != reading.beacon
             })
