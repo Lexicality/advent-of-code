63c89d1f7adefd635734b35c7f7c185481fe9179
Lex Robinson <lex@lex.me.uk>
2023-12-18 19:38:18 +0000

    2023 17.2

--- src/aoc/y2023/day17.rs
+++ src/aoc/y2023/day17.rs
@@ -73,7 +73,7 @@ pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     let grid: Grid<u32> = Grid::new_from_lines(
         data.map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec()),
     );
-    println!("{grid:}");
+    // println!("{grid:#}");
     let start = grid.min_key();
     let end = grid.max_key();
 
@@ -87,23 +87,39 @@ pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
 
     queue.push_back(start_id);
 
-    const MAX_STEPPE: u32 = 3;
+    const MIN_STEPPE: u32 = 4;
+    const MAX_STEPPE: u32 = 10;
 
     // Generate the graph
     while let Some((pos, dir, steps)) = queue.pop_front() {
         let next_steps = [
-            (pos + dir.to_coord(), dir, steps + 1),
             {
-                let dir = dir.rotate(RotateDirection::Left);
-                (pos + dir.to_coord(), dir, 1)
+                if steps < MAX_STEPPE {
+                    Some((pos + dir.to_coord(), dir, steps + 1))
+                } else {
+                    None
+                }
             },
             {
-                let dir = dir.rotate(RotateDirection::Right);
-                (pos + dir.to_coord(), dir, 1)
+                if steps >= MIN_STEPPE {
+                    let dir = dir.rotate(RotateDirection::Left);
+                    Some((pos + dir.to_coord(), dir, 1))
+                } else {
+                    None
+                }
+            },
+            {
+                if steps >= MIN_STEPPE {
+                    let dir = dir.rotate(RotateDirection::Right);
+                    Some((pos + dir.to_coord(), dir, 1))
+                } else {
+                    None
+                }
             },
         ]
         .into_iter()
-        .filter(|(pos, _, steps)| *steps <= MAX_STEPPE && grid.check_coord(pos))
+        .flatten()
+        .filter(|(pos, _, steps)| grid.check_coord(pos) && (pos != &end || *steps >= MIN_STEPPE))
         .collect_vec();
         let node = Node::new(
             (pos, dir, steps),
@@ -143,7 +159,7 @@ pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
                 grid.set(id.0, FinalGridState::Trampled(id.1));
                 id = came_from.get(id).unwrap();
                 if id == &start_id {
-                    println!("{grid:.>}");
+                    println!("{grid}");
                     return Ok(ret.to_string());
                 }
             }
