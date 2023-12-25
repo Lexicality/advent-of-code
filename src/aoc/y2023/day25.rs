34f8d0938045c68ac0fb7b5051a8c3e39f4ccbbd
Lexi Robinson <lexi@lexi.org.uk>
2023-12-25 18:53:23 +0000

    2023 25.1 WIP

---
+++ src/aoc/y2023/day25.rs
@@ -0,0 +1,79 @@
+use std::{collections::HashMap, str::FromStr};
+
+use itertools::Itertools;
+
+use crate::AoCError;
+
+#[derive(Debug)]
+struct Node {
+    name: String,
+    parents: Vec<String>,
+    children: Vec<String>,
+}
+
+impl FromStr for Node {
+    type Err = AoCError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (name, children) = s
+            .split_once(':')
+            .ok_or_else(|| AoCError::new("Expected a : but didn't find one"))?;
+        Ok(Self {
+            name: name.to_owned(),
+            parents: Vec::new(),
+            children: children.trim().split(' ').map(|s| s.to_owned()).collect(),
+        })
+    }
+}
+
+impl Node {
+    fn mew(name: String, parent: String) -> Self {
+        Self {
+            name,
+            parents: vec![parent.to_owned()],
+            children: Vec::new(),
+        }
+    }
+}
+
+pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
+    let nodes: Vec<Node> = data.map(|line| line.parse()).try_collect()?;
+    let mut nodes: HashMap<String, Node> = nodes
+        .into_iter()
+        .map(|node| (node.name.clone(), node))
+        .collect();
+
+    let parenting = nodes
+        .values()
+        .flat_map(|node| {
+            node.children
+                .iter()
+                .cloned()
+                .map(|child| (node.name.clone(), child))
+                .collect_vec()
+        })
+        .collect_vec();
+
+    for (parent, child) in parenting.into_iter() {
+        match nodes.get_mut(&child) {
+            Some(node) => {
+                node.parents.push(parent);
+            }
+            None => {
+                nodes.insert(child.clone(), Node::mew(child, parent));
+            }
+        }
+    }
+
+    println!("{nodes:#?}");
+
+    let ret = 0;
+    Ok(ret.to_string())
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2023",
+    day: "25",
+    func: main,
+    example_func: None,
+});
