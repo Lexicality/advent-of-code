3792a5d7660bd19e42a9975a9febfaddc47b7a24
Lexi Robinson <lexi@lexi.org.uk>
2023-12-27 07:56:49 +0000

    2015 02.2

--- src/aoc/y2015/day02.rs
+++ src/aoc/y2015/day02.rs
@@ -3,16 +3,16 @@ use itertools::Itertools;
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     let ret: u32 = data
         .map(|line| {
-            let tmp: Vec<u32> = line.split('x').map(|c| c.parse()).try_collect().unwrap();
-
+            let mut tmp: Vec<u32> = line.split('x').map(|c| c.parse()).try_collect().unwrap();
+            tmp.sort();
             let (l, w, h) = tmp.into_iter().collect_tuple().unwrap();
 
-            let a = l * w;
-            let b = w * h;
-            let c = l * h;
+            let a = (l, w);
+            let b = (w, h);
+            let c = (l, h);
             let slack = a.min(b).min(c);
 
-            2 * a + 2 * b + 2 * c + slack
+            2 * slack.0 + 2 * slack.1 + l * w * h
         })
         .sum();
     Ok(ret.to_string())
