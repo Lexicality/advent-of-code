e75c2906ec47db8c94547e3cfee49f08c55d8bf3
Lexi Robinson <lexi@lexi.org.uk>
2024-01-01 15:18:37 +0000

    2015 05.1

---
+++ src/aoc/y2015/day05.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+
+const NAUGHTY_STRINGS: [&str; 4] = ["ab", "cd", "pq", "xy"];
+const VOWELS: [char; 5] = ['a', 'o', 'e', 'u', 'i'];
+
+pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
+    let ret = data
+        .filter(|line| {
+            for naughty in NAUGHTY_STRINGS.iter() {
+                if line.contains(naughty) {
+                    return false;
+                }
+            }
+            let vowels = line.chars().filter(|c| VOWELS.contains(c)).count() >= 3;
+            let doubles = line.chars().tuple_windows().any(|(a, b)| a == b);
+            vowels && doubles
+        })
+        .count();
+    Ok(ret.to_string())
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2015",
+    day: "5",
+    func: main,
+    example_func: None,
+});
