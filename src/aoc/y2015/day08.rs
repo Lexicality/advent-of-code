1ded37d274f156cd803b8f44eb75207fccb1bca9
Lexi Robinson <lexi@lexi.org.uk>
2024-01-01 19:09:24 +0000

    2015 08.2

--- src/aoc/y2015/day08.rs
+++ src/aoc/y2015/day08.rs
@@ -1,35 +1,7 @@
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     let mut ret = 0;
     for line in data {
-        let mut line = line.as_str();
-        ret += line.len();
-        line = line.strip_prefix('"').expect("Must have started with \"");
-        line = line.strip_suffix('"').expect("Must have started with \"");
-        let mut in_escape = false;
-        let mut skip_n = 0;
-        let mut strlen = 0;
-
-        for c in line.chars() {
-            if skip_n > 0 {
-                skip_n -= 1;
-                continue;
-            }
-            if !in_escape {
-                if c == '\\' {
-                    in_escape = true
-                } else {
-                    strlen += 1;
-                }
-            } else {
-                in_escape = false;
-                strlen += 1;
-                if c == 'x' {
-                    skip_n += 2;
-                }
-            }
-        }
-
-        ret -= strlen;
+        ret += line.escape_debug().to_string().len() + 2 - line.len();
     }
     Ok(ret.to_string())
 }
