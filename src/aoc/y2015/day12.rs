e9992c24f99aca0856c755615b41aef17a375b13
Lexi Robinson <lexi@lexi.org.uk>
2024-01-14 00:17:09 +0000

    2015 12.1

---
+++ src/aoc/y2015/day12.rs
@@ -0,0 +1,28 @@
+use serde_json::Value as JSONValue;
+
+fn recursive_sum(value: JSONValue) -> i64 {
+    match value {
+        JSONValue::Bool(_) | JSONValue::Null | JSONValue::String(_) => 0,
+        JSONValue::Number(i) => i.as_i64().unwrap(),
+        JSONValue::Array(arr) => arr.into_iter().map(recursive_sum).sum(),
+        JSONValue::Object(obj) => obj.into_iter().map(|(_, v)| v).map(recursive_sum).sum(),
+    }
+}
+
+pub fn main(mut data: crate::DataIn) -> crate::AoCResult<String> {
+    let line = data.next().unwrap();
+    let value = serde_json::from_str(&line).unwrap();
+    let ret = recursive_sum(value);
+    Ok(ret.to_string())
+}
+
+pub fn main_example(data: crate::DataIn) -> crate::AoCResult<String> {
+    crate::multi_line_example(data, main)
+}
+
+inventory::submit!(crate::AoCDay::mew_with_example(
+    "2015",
+    "12",
+    main,
+    main_example
+));
