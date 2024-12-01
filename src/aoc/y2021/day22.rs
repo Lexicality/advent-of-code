a5a63c24a1c31bd2e3632e1fae48c337c0dd6ce4
Lexi Robinson <lexi@lexi.org.uk>
2024-12-01 18:37:03 +0000

    2021 22.1

---
+++ src/aoc/y2021/day22.rs
@@ -0,0 +1,98 @@
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use crate::{AoCError, AoCResult, CommonGrid, Coord3D, InfGrid};
+
+#[derive(Debug, Clone, Copy)]
+enum CubeState {
+    Off,
+    On,
+}
+impl CubeState {
+    fn parse(input: &str) -> AoCResult<Self> {
+        Ok(match input {
+            "on" => Self::On,
+            "off" => Self::Off,
+            _ => return Err(AoCError::new(format!("Unknown cube state '{input}'"))),
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+struct RebootStep {
+    state: CubeState,
+    min: Coord3D,
+    max: Coord3D,
+}
+
+impl RebootStep {
+    fn parse(line: String) -> AoCResult<Self> {
+        lazy_static! {
+            static ref LINE_RE: Regex =
+                Regex::new(r"^(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)$")
+                    .unwrap();
+        }
+
+        let matches = LINE_RE
+            .captures(&line)
+            .ok_or_else(|| AoCError::new(format!("line {line} doesn't match regex")))?;
+        Ok(Self {
+            state: CubeState::parse(&matches[1])?,
+            min: Coord3D {
+                x: matches[2].parse().map_err(AoCError::new_from_parseerror)?,
+                y: matches[4].parse().map_err(AoCError::new_from_parseerror)?,
+                z: matches[6].parse().map_err(AoCError::new_from_parseerror)?,
+            },
+            max: Coord3D {
+                x: matches[3].parse().map_err(AoCError::new_from_parseerror)?,
+                y: matches[5].parse().map_err(AoCError::new_from_parseerror)?,
+                z: matches[7].parse().map_err(AoCError::new_from_parseerror)?,
+            },
+        })
+    }
+
+    fn get_coords(&self) -> impl Iterator<Item = Coord3D> {
+        (self.min.x..=self.max.x)
+            .cartesian_product(self.min.y..=self.max.y)
+            .cartesian_product(self.min.z..=self.max.z)
+            .map(|((x, y), z)| Coord3D { x, y, z })
+    }
+
+    fn is_legit(&self) -> bool {
+        // const MAGIC_SIZE: std::ops::RangeInclusive<i32> = -50..=50;
+
+        (self.min.x <= 50 && self.max.x >= -50)
+            && (self.min.y <= 50 && self.max.y >= -50)
+            && (self.min.z <= 50 && self.max.z >= -50)
+    }
+}
+
+pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
+    let mut grid: InfGrid<CubeState, Coord3D> = InfGrid::new();
+
+    for line in data {
+        let step = RebootStep::parse(line)?;
+        if !(step.is_legit()) {
+            continue;
+        }
+        println!("step: {step:?}");
+        for coord in step.get_coords()
+        // .filter(|coord| {
+        //     MAGIC_SIZE.contains(&coord.x)
+        //         && MAGIC_SIZE.contains(&coord.y)
+        //         && MAGIC_SIZE.contains(&coord.z)
+        // })
+        {
+            grid.set(coord, step.state);
+        }
+    }
+
+    let ret = grid
+        .into_iter()
+        .filter(|(_, v)| matches!(v, CubeState::On))
+        .count();
+    Ok(ret.to_string())
+}
+
+inventory::submit!(crate::AoCDay::mew("2021", "22", main));
--- src/aoc/y2021/mod.rs
+++ src/aoc/y2021/mod.rs
@@ -1,2 +1,3 @@
 pub mod day20;
 pub mod day21;
+pub mod day22;
