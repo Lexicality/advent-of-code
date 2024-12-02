46163cc96275008f814620bfb0c0e0c83ae92b24
Lexi Robinson <lexi@lexi.org.uk>
2024-12-02 10:31:39 +0000

    2024 02.2 failed??

--- src/aoc/y2024/day02.rs
+++ src/aoc/y2024/day02.rs
@@ -1,48 +1,84 @@
-use itertools::Itertools;
-
 enum Direction {
     Mystery,
     Increasing,
     Decreasing,
 }
 
-pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
-    let mut ret = 0;
-    'lines: for line in data {
-        print!("{line} ");
-        let mut dir = Direction::Mystery;
-        for (a, b) in line
-            .split(' ')
-            .map(|c| c.parse::<u32>().unwrap())
-            .tuple_windows()
-        {
-            if a.abs_diff(b) > 3 {
-                println!("UNSAFE (too big a change)");
-                continue 'lines;
+fn check_line(line: &str, mut dampening: bool) -> bool {
+    print!("{line} || ");
+    let mut dir = Direction::Mystery;
+    let mut results = line.split(' ').map(|c| c.parse::<u32>().unwrap());
+
+    let mut a = results.next().unwrap();
+    print!("{a} ");
+
+    for b in results {
+        print!("{b} ");
+        if a.abs_diff(b) > 3 {
+            print!("(ERR |{a} - {b}| == {} > 3!) ", a.abs_diff(b));
+            if !dampening {
+                print!("damp! ");
+                dampening = true;
+                continue;
             }
-            match a.cmp(&b) {
-                std::cmp::Ordering::Less => {
-                    if matches!(dir, Direction::Increasing) {
-                        println!("UNSAFE (dir change)");
-                        continue 'lines;
+            return false;
+        }
+        match a.cmp(&b) {
+            std::cmp::Ordering::Less => {
+                if matches!(dir, Direction::Increasing) {
+                    print!("(ERR {a} < {b}!) ");
+                    if !dampening {
+                        print!("damp! ");
+                        dampening = true;
+                        continue;
                     }
-                    dir = Direction::Decreasing
+                    return false;
                 }
-                std::cmp::Ordering::Equal => {
-                    println!("UNSAFE (equality)");
-                    continue 'lines;
+                dir = Direction::Decreasing
+            }
+            std::cmp::Ordering::Equal => {
+                print!("(ERR {a} == {b}!) ");
+                if !dampening {
+                    print!("damp! ");
+                    dampening = true;
+                    continue;
                 }
-                std::cmp::Ordering::Greater => {
-                    if matches!(dir, Direction::Decreasing) {
-                        println!("UNSAFE (dir change)");
-                        continue 'lines;
+                return false;
+            }
+            std::cmp::Ordering::Greater => {
+                if matches!(dir, Direction::Decreasing) {
+                    print!("(ERR {a} > {b}!) ");
+                    if !dampening {
+                        print!("damp! ");
+                        dampening = true;
+                        continue;
                     }
-                    dir = Direction::Increasing
+                    return false;
                 }
+                dir = Direction::Increasing
             }
         }
-        println!("safe!");
-        ret += 1;
+        a = b;
+    }
+    true
+}
+
+pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
+    let mut ret = 0;
+    for line in data {
+        if check_line(&line, false) {
+            ret += 1;
+            println!("safe!");
+            continue;
+        }
+        println!("RETRY");
+        let (_, line) = line.split_once(' ').unwrap();
+        if check_line(line, true) {
+            println!("safe!");
+            ret += 1;
+            continue;
+        }
+        println!("UNSAFE");
     }
     Ok(ret.to_string())
 }
