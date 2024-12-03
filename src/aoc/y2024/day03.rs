3c8c84321a2132807255afb3273946f411fbda40
Lexi Robinson <lexi@lexi.org.uk>
2024-12-03 08:34:26 +0000

    2024 03.2

--- src/aoc/y2024/day03.rs
+++ src/aoc/y2024/day03.rs
@@ -4,15 +4,22 @@ use regex::Regex;
 
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     lazy_static! {
-        static ref LINE_RE: Regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+        static ref LINE_RE: Regex = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
     }
 
     let mut ret = 0;
+    let mut dont = false;
     for line in data {
-        for [a, b] in LINE_RE.captures_iter(&line).map(|c| c.extract().1) {
-            let a: u64 = a.parse().map_err(AoCError::new_from_parseerror)?;
-            let b: u64 = b.parse().map_err(AoCError::new_from_parseerror)?;
-            ret += a * b;
+        for capture in LINE_RE.captures_iter(&line) {
+            if &capture[0] == "don't()" {
+                dont = true;
+            } else if &capture[0] == "do()" {
+                dont = false;
+            } else if !dont {
+                let a: u64 = capture[1].parse().map_err(AoCError::new_from_parseerror)?;
+                let b: u64 = capture[2].parse().map_err(AoCError::new_from_parseerror)?;
+                ret += a * b;
+            }
         }
     }
     Ok(ret.to_string())
