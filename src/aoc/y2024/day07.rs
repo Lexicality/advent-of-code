0e3960ff2e0b07f81b6c37b8da629979f594d279
Lexi Robinson <lexi@lexi.org.uk>
2024-12-08 13:24:33 +0000

    2024 07.1

--- src/aoc/y2024/day07.rs
+++ src/aoc/y2024/day07.rs
@@ -4,6 +4,13 @@ use itertools::Itertools;
 
 use crate::AoCError;
 
+const OPERATIONS: [fn(u64, u64) -> u64; 2] = [
+    // Add
+    |a: u64, b: u64| a + b,
+    // Multiply
+    |a: u64, b: u64| a * b,
+];
+
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     let calibrations: HashMap<u64, Vec<u64>> = data
         .map(|line| {
@@ -21,10 +28,23 @@ pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
     let ret: u64 = calibrations
         .into_iter()
         .filter_map(|(result, nums)| {
-            if nums.len() > 1 {
-                return None;
+            let mut nums = nums.into_iter();
+            let mut options = vec![nums.next().unwrap()];
+            for num in nums {
+                options = options
+                    .into_iter()
+                    .flat_map(|option| {
+                        OPERATIONS
+                            .iter()
+                            .map(move |operation| operation(option, num))
+                    })
+                    .filter(|option| option <= &result)
+                    .collect();
             }
-            Some(result)
+            options
+                .into_iter()
+                .any(|res| res == result)
+                .then_some(result)
         })
         .sum();
     Ok(ret.to_string())
