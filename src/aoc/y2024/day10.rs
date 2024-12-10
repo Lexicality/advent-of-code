ebaa73b506621c73365baf0375ec0fd5e3b7e55d
Lexi Robinson <lexi@lexi.org.uk>
2024-12-10 10:04:19 +0000

    2024 10.1

---
+++ src/aoc/y2024/day10.rs
@@ -0,0 +1,78 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    fmt::Display,
+};
+
+use crate::{AoCError, CharGrid, CommonGrid, Grid};
+
+struct Height(u32);
+
+impl TryFrom<char> for Height {
+    type Error = AoCError;
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        Ok(match value {
+            '.' => Self(u32::MAX),
+            c if c.is_ascii_digit() => Self(c.to_digit(10).unwrap()),
+            _ => return Err(AoCError::new_from_char(value)),
+        })
+    }
+}
+
+impl Display for Height {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            c if c < 10 => c.fmt(f),
+            _ => '.'.fmt(f),
+        }
+    }
+}
+
+impl Height {
+    fn can_walk(&self, from: &Self) -> bool {
+        self.0 > from.0 && (self.0 - from.0) == 1
+    }
+
+    fn is_trailhead(&self) -> bool {
+        self.0 == 0
+    }
+
+    fn is_end(&self) -> bool {
+        self.0 == 9
+    }
+}
+
+pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
+    let grid: Grid<Height> = Grid::new_from_chars(data)?;
+
+    let mut ret = 0;
+    for trailhead in grid
+        .iter()
+        .filter(|(_, h)| h.is_trailhead())
+        .map(|(c, _)| c)
+        .copied()
+    {
+        // Ungabunga brepth first here we go
+        let mut options = VecDeque::with_capacity(grid.len());
+        options.push_front(trailhead);
+
+        let mut seen = HashSet::with_capacity(grid.len());
+        while let Some(coord) = options.pop_front() {
+            if !seen.insert(coord) {
+                continue;
+            }
+            let value = grid.get(&coord).unwrap();
+            if value.is_end() {
+                ret += 1;
+                continue;
+            }
+            options.extend(
+                grid.get_neighbour_coords_filtered(coord, false, |coord, next| {
+                    next.can_walk(value) && !seen.contains(coord)
+                }),
+            );
+        }
+    }
+    Ok(ret.to_string())
+}
+
+inventory::submit!(crate::AoCDay::mew("2024", "10", main));
