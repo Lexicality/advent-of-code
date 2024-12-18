62c21efe5f64b63391da15ad4587c9977a668d79
Lexi Robinson <lexi@lexi.org.uk>
2024-12-18 10:25:24 +0000

    2024 18.2

--- src/aoc/y2024/day18.rs
+++ src/aoc/y2024/day18.rs
@@ -9,39 +9,46 @@
 
 use std::fmt::Display;
 
+use itertools::Itertools;
+
 use crate::utils::astar::{a_star, AStarProvider};
 use crate::{symbols, AoCResult, CommonGrid, Coord2D, Coordinate, Direction, Grid};
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone, Copy)]
+#[allow(dead_code)]
 enum GridState {
     #[default]
     Void,
     Corrupted(usize),
+    Walko,
 }
 
 impl Display for GridState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             GridState::Void => symbols::VOID,
-            GridState::Corrupted(_) => symbols::BLOCK,
+            GridState::Corrupted(i) if *i < 12 => symbols::BLOCK,
+            GridState::Corrupted(_) => symbols::SHADE_LIGHT,
+            GridState::Walko => symbols::BOX,
         }
         .fmt(f)
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct AStarImpl {
     grid: Grid<GridState>,
     end: Coord2D,
+    initial_iterations: usize,
 }
 
-type AStarID = (Coord2D, usize);
+type AStarID = Coord2D;
 
 impl AStarImpl {
     fn new(data: crate::DataIn, width: u32, iterations: usize) -> AoCResult<Self> {
         let mut grid = Grid::new(width, width);
 
-        for (i, coord) in data.enumerate().take(iterations) {
+        for (i, coord) in data.enumerate() {
             let coord = coord.parse()?;
             grid.set(coord, GridState::Corrupted(i));
         }
@@ -50,11 +57,12 @@ impl AStarImpl {
         Ok(Self {
             grid,
             end: Coord2D { x: end, y: end },
+            initial_iterations: iterations,
         })
     }
 
     fn get_start(&self) -> AStarID {
-        (Coord2D { x: 0, y: 0 }, 0)
+        Coord2D { x: 0, y: 0 }
     }
 }
 
@@ -68,7 +76,7 @@ impl AStarProvider for AStarImpl {
     type IDType = AStarID;
 
     fn get_neighbours(&self, id: &Self::IDType) -> Box<dyn Iterator<Item = Self::IDType> + '_> {
-        let (coord, steps) = *id;
+        let coord = *id;
         Box::new(
             [
                 Direction::North,
@@ -77,17 +85,18 @@ impl AStarProvider for AStarImpl {
                 Direction::West,
             ]
             .into_iter()
-            .map(move |direction| (coord + direction.to_coord(), steps + 1))
-            .filter(move |(coord, _steps)| {
+            .map(move |direction| (coord + direction.to_coord()))
+            .filter(move |coord| {
                 self.grid.get(coord).is_some_and(|value| match value {
                     GridState::Void => true,
-                    GridState::Corrupted(_falls_at) => false, //*_falls_at > steps,
+                    GridState::Corrupted(falls_at) => *falls_at >= self.initial_iterations,
+                    GridState::Walko => unreachable!(),
                 })
             }),
         )
     }
 
-    fn heuristic(&self, (coord, _): &Self::IDType) -> u64 {
+    fn heuristic(&self, coord: &Self::IDType) -> u64 {
         self.end.distance(coord).try_into().unwrap()
     }
 
@@ -95,7 +104,7 @@ impl AStarProvider for AStarImpl {
         1
     }
 
-    fn is_end(&self, (coord, _): &Self::IDType) -> bool {
+    fn is_end(&self, coord: &Self::IDType) -> bool {
         coord == &self.end
     }
 }
@@ -103,9 +112,35 @@ impl AStarProvider for AStarImpl {
 fn puzzle(data: crate::DataIn, width: u32, iterations: usize) -> AoCResult<String> {
     let provider = AStarImpl::new(data, width, iterations)?;
     let start = provider.get_start();
-    let res = a_star(provider, start);
-    let ret = res.len();
-    Ok(ret.to_string())
+
+    // I'm a genius 🤦
+    let bytes: Vec<Coord2D> = provider
+        .grid
+        .iter()
+        .filter_map(|(c, v)| match v {
+            GridState::Corrupted(i) => Some((i, c)),
+            _ => None,
+        })
+        .sorted_by_cached_key(|v| v.0)
+        .map(|(_, c)| *c)
+        .collect();
+
+    // deeply, deeply regretting my API design choices with the a_star function
+    for (i, coord) in bytes.into_iter().enumerate().skip(iterations) {
+        let mut provider = provider.clone();
+        provider.initial_iterations = i + 1;
+        // let mut griddo = provider.grid.clone();
+        let res = a_star(provider, start);
+        if res.is_empty() {
+            return Ok(coord.to_string());
+        }
+        // for coord in res {
+        //     griddo.set(coord, GridState::Walko);
+        // }
+        // println!("{coord}:\n{griddo}");
+    }
+
+    unreachable!()
 }
 
 pub fn main(data: crate::DataIn) -> crate::AoCResult<String> {
