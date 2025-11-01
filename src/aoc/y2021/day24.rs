b6fa7131251e81eb7baeb32911f14337b9359df2
Lexi Robinson <lexi@lexi.org.uk>
2025-11-01 10:12:44 +0000

    Fix incorrect shebangs
    Also coincidentally, an unlicensed file.

--- src/aoc/y2021/day24.rs
+++ src/aoc/y2021/day24.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code, unused_variables, unused_mut)]
 // Copyright (c) 2024 Lexi Robinson
 //
 // Licensed under the EUPL, Version 1.2
@@ -7,6 +6,8 @@
 // You should have received a copy of the Licence along with this work. If not, see:
 // <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
 // See the Licence for the specific language governing permissions and limitations under the Licence.
+
+#![allow(dead_code, unused_variables, unused_mut)]
 
 use itertools::Itertools;
 
