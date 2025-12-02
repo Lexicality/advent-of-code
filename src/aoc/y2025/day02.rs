7827550718fc5fd3d7bf17b228cbd25385ed289b
Lexi Robinson <lexi@lexi.org.uk>
2025-12-02 15:09:36 +0000

    2025 02.2 | less stupid window function

--- src/aoc/y2025/day02.rs
+++ src/aoc/y2025/day02.rs
@@ -62,17 +62,14 @@ pub fn part_2(data: crate::DataIn) -> crate::AoCResult<String> {
                 let len = numstr.len();
                 let max_len = len / 2;
 
-                // This takes 4 seconds but whatever 😭
                 (1..=max_len)
                     // Filter out any number that can't make valid windows
                     .filter(|size| len % size == 0)
                     .any(|size| {
                         let substr = &numstr[0..size];
-                        numstr
-                            .chars()
-                            .chunks(size)
-                            .into_iter()
-                            .all(|window| window.collect::<String>() == substr)
+                        (size..=len - size)
+                            .step_by(size)
+                            .all(|start| &numstr[start..start + size] == substr)
                     })
             })
         })
