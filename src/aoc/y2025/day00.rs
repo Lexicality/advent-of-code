04f890f5f450a5921b87b26a36e7a3644bf6f5bc
Lexi Robinson <lexi@lexi.org.uk>
2025-12-04 08:21:53 +0000

    Put day00 back
    oops

---
+++ src/aoc/y2025/day00.rs
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Lexi Robinson
+//
+// Licensed under the EUPL, Version 1.2
+//
+// You may not use this work except in compliance with the Licence.
+// You should have received a copy of the Licence along with this work. If not, see:
+// <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
+// See the Licence for the specific language governing permissions and limitations under the Licence.
+
+pub fn part_1(data: crate::DataIn) -> crate::AoCResult<String> {
+    let ret = data.count();
+    Ok(ret.to_string())
+}
+
+inventory::submit!(crate::AoCDay {
+    year: "2025",
+    day: "0",
+    part_1: crate::AoCPart {
+        main: part_1,
+        example: part_1
+    },
+    part_2: None
+});
