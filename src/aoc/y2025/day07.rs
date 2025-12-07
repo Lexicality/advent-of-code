aef34db5280640458f73fdf260d0b79d427cdf6e
Lexi Robinson <lexi@lexi.org.uk>
2025-12-07 17:44:49 +0000

    2025 07.2
    Victory for "almost exactly solving part 2 in part 1 because you didn't
    read it properly" once again

--- src/aoc/y2025/day07.rs
+++ src/aoc/y2025/day07.rs
@@ -6,7 +6,7 @@
 // You should have received a copy of the Licence along with this work. If not, see:
 // <https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12>.
 // See the Licence for the specific language governing permissions and limitations under the Licence.
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 
 use aoc_macros::VoidState;
@@ -91,6 +91,39 @@ pub fn part_1(data: crate::DataIn) -> crate::AoCResult<String> {
     Ok(ret.to_string())
 }
 
+pub fn part_2(data: crate::DataIn) -> crate::AoCResult<String> {
+    let grid = SparseGrid::<GridState>::new_from_chars(
+        data
+            // get rid of the empty lines since they don't do anything
+            .filter(|line| !line.chars().all(|c| c == '.')),
+    )?;
+    let max = grid.max_key();
+
+    let ret: u64 = (1..=max.y)
+        .fold(
+            grid.iter()
+                .filter(|(_, state)| matches!(state, GridState::Start))
+                .map(|(coord, _)| (*coord, 1))
+                .collect(),
+            |beams: HashMap<Coord2D, u64>, y| {
+                let mut new_beams = HashMap::with_capacity(beams.len());
+                for (mut coord, count) in beams.into_iter() {
+                    coord.y = y;
+                    if matches!(grid.get(&coord), Some(GridState::Splitter)) {
+                        *new_beams.entry(coord + Direction::East.into()).or_default() += count;
+                        *new_beams.entry(coord + Direction::West.into()).or_default() += count;
+                    } else {
+                        *new_beams.entry(coord).or_default() += count;
+                    }
+                }
+                new_beams
+            },
+        )
+        .values()
+        .sum();
+    Ok(ret.to_string())
+}
+
 inventory::submit!(crate::AoCDay {
     year: "2025",
     day: "7",
@@ -98,5 +131,8 @@ inventory::submit!(crate::AoCDay {
         main: part_1,
         example: part_1
     },
-    part_2: None
+    part_2: Some(crate::AoCPart {
+        main: part_2,
+        example: part_2
+    })
 });
